98c509901a10735a600ac3544d6396d086b22558
Sayantan <sayantan@hp.com>
2020-11-01 20:13:20 -0500

    Added Basic Folder Structure

---
+++ LocationManager/OpenStreetMap.py
@@ -0,0 +1,17 @@
+class OpenStreetMap():
+    def __init__(self):
+        pass 
+    
+    def getGraph(self, location='Amherst'):
+        pass 
+    
+    
+    def getCordinates(self):
+        pass 
+    
+    
+    def setBoundedBox(self):
+        pass 
+    
+    
+    

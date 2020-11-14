ea304e909203b40f622323572d40d5d552a834cc
Sayantan <sayantan@hp.com>
2020-11-13 19:21:02 -0500

    Added additional code

--- MapMgr/ProcessMap.py
+++ MapMgr/ProcessMap.py
@@ -1,17 +1,22 @@
-'''
-Created on Nov 13, 2020
-
-
-'''
-
+from geopy.geocoders import Nominatim
 class ProcessMap(object):
     '''
-    classdocs
+    Class to interact with osmnx api and translate address to required format
     '''
 
 
-    def __init__(self, params):
-        '''
-        Constructor
-        '''
+    def __init__(self):
+        self.geolocator = Nominatim(user_agent='elena')
         
+    
+    def processLocationParams(self, location):
+        locationParams = location.split(',')
+        
+        params = dict()
+        params['town'] = locationParams[1].strip()
+        params['state'] = locationParams[2].strip()
+        params['latitude'] = self.geolocator.geocode(location).latitude
+        params['longitude'] = self.geolocator.geocode(location).longitude
+        
+        return params
+    
--- RoutingMgr/ProcessRoute.py
+++ RoutingMgr/ProcessRoute.py
@@ -1,17 +1,27 @@
-'''
-Created on Nov 13, 2020
+from MapMgr.ProcessMap import ProcessMap
 
+from flask import request, jsonify
 
-'''
 
 class ProcessRoute(object):
-    '''
-    classdocs
-    '''
 
-
-    def __init__(self, params):
-        '''
-        Constructor
-        '''
+    def __init__(self):
+        self.processMap = ProcessMap()
         
+    
+    def getRoute(self):
+        payload = request.get_json()
+        
+        source = payload['source']
+        destination  = payload['destination']
+        boolIsMax = bool(payload['maxMin']=='max')
+        percentage = float(payload['percentage'])
+        
+        srcParams = self.processMap.processLocationParams(source)
+        destParams = self.processMap.processLocationParams(destination)
+        
+        route, dist, elevation = self.findRoute(srcParams, destParams, percentage, boolIsMax)
+        
+        
+    def findRoute(self, srcParams, destParams, percentage, isMax):
+        pass 

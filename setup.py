c759505bcc5c9bc054f42deb36bed0f854446cfa
Sayantan <sayantan@hp.com>
2020-11-16 18:27:35 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,15 @@
-#!/usr/bin/env python
 import os
-# import glob
 
 from setuptools import setup, find_packages
 
 import Sources
 
-#import ProfileManager
 
-try:
-    long_description = open('README.rst', 'rt').read()
-except IOError:
-    long_description = ''
-   
-
-   
 pkg_scripts = []
 
 setup(
     name=Sources.__name__,
-        long_description=long_description,
+        long_description='ELena',
 
    
     platforms=['Any'],
@@ -30,15 +20,7 @@ setup(
     include_package_data=True,
     package_data={'Sources' : []},
 
-#     entry_points={
-#         'console_scripts': [
-#             'profilemanager = ProfileManager.Apps.ProfileManagerApp:main'
-#         ],
-#         'ProfileManager.commands': [
-#             'restserver = ProfileManager.Commands.RestServer:RestServer',
-#             'daemon = ProfileManager.Commands.TestGroupGenerator:TestGroupGenerator'
-#         ],
-#     },
+
 
     zip_safe=False,
 )

870b65e77ece91a7e907baea1bcf302d7e404944
Sayantan <sayantan@hp.com>
2020-11-16 14:50:23 -0500

    Refactored algo code from lamson repo

--- MapMgr/ProcessMap.py
+++ MapMgr/ProcessMap.py
@@ -1,5 +1,37 @@
 import osmnx
 
+import osmnx as ox
+import pickle as pkl
+from pathlib import Path
+import os
+import requests
+
+import math
+import time
+import requests
+import pandas as pd
+import networkx as nx
+
+from osmnx.core import save_to_cache
+from osmnx.core import get_from_cache
+from osmnx.utils import log
+
+import sys
+import osmnx as ox
+import networkx as nx
+import numpy as np
+from heapq import *
+from itertools import count
+
+from heapq import *
+from collections import OrderedDict
+import numpy as np
+
+
+
+
+
+
 from geopy.geocoders import Nominatim
 
 from RoutingMgr.ProcessRoute import ProcessRoute
@@ -35,6 +67,137 @@ class ProcessMap(object):
     
     def getNearestNode(self, graph, latitude, longitude):
         return osmnx.get_nearest_node(graph, latitude, longitude)
+    
+    
+    
+    
+    def getElevationGain(self, graph, start, end):
+        if start == end:
+            return 0
+        return graph.nodes[start]['elevation'] - graph.nodes[end]['elevation']
+
+
+    def getLength(self,graph, start, end):
+        return graph.edges[start, end, 0]['length']
+    
+    
+    def getpath(self, revPath, origin, destination):
+        route_by_length_minele = []
+        p = destination
+        route_by_length_minele.append(p)
+        while p != origin:
+            p = revPath[p]
+            route_by_length_minele.append(p)
+        route_by_length_minele = route_by_length_minele[::-1]
+        return route_by_length_minele
+    
+    
+    def generatePath(self, revPath, start, end):
+        path = []
+        n = end
+        path.append(n)
+        while n != start:
+            n = revPath[n]
+            path.append(n)
+        return path[::-1]
+    
+    
+    def getPathElevation(self, graph, path):
+        total_elevation = 0
+    
+        for i in range(len(path) - 1):
+            curr_elevation = self.getElevationGain(graph, path[i], path[i + 1])
+            if curr_elevation > 0:
+                total_elevation += curr_elevation
+    
+        return total_elevation
+
+
+    def getPathLength(self, graph , path):
+        total_length = 0
+    
+        for i in range(len(path) - 1):
+            total_length += self.getLength(graph, path[i], path[i + 1])
+    
+        return total_length
+
+    
+    def getLatLong(self,graph, path):
+        coord = []
+        for node in path:
+            coord.append((graph.nodes[node]['y'], graph.nodes[node]['x']))
+        return coord
+
+
+    def getShortestPath(self, graph, start, end, option='length'):
+        queue = []
+        heappush(queue, (0, start))
+        revPath = {}
+        cost = {}
+        revPath[start] = None
+        cost[start] = 0
+    
+        while len(queue) > 0:
+            (val, current) = heappop(queue)
+            if current == end:
+                break
+            for cur, nxt, data in graph.edges(current, data=True):
+                cur_cost = cost[current]
+                if option == 'length':
+                    curCost = self.getLength(graph, cur, nxt)
+#                 elif option == 'elevation':
+#                     # this part mostly doesn't even work
+#                     curCost = self.getPathElevation(graph, cur, nxt)
+                if curCost > 0:
+                    cur_cost += curCost
+                if nxt not in cost or cur_cost < cost[nxt]:
+                    cost[nxt] = cur_cost
+                    heappush(queue, (cur_cost, nxt))
+                    revPath[nxt] = current
+    
+        return self.generatePath(revPath, start, end)
+    
+
+    def getEucledeanDistance(self, graph, start, end):
+        x1, y1 = graph.nodes()[start]['x'], graph.nodes()[start]['y']
+        x2, y2 = graph.nodes()[end]['x'], graph.nodes()[end]['y']
+    
+        dist = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+    
+        return dist
+    
+
+    def getFromAllPaths(self,G, start, end, percent, max_ele=True):
+        min_distance = self.getPathLength(G, self.getShortestPath(G, start, end))
+        shortest_paths = list(nx.all_shortest_paths(G, start, end))
+    
+        print(percent)
+        max_path_length = (1.0 + float(percent)) * min_distance
+    
+        elevation_gain = {}
+        for p in shortest_paths:
+            path_dist = self.getPathLength(G, p)
+            if path_dist > max_path_length:
+                print(min_distance, max_path_length)
+                continue
+            elevation_gain[self.getPathElevation(G, p)] = p
+    
+        ordered_paths = OrderedDict(sorted(elevation_gain.items()))
+    
+        keys = ordered_paths.keys()
+    
+        if max_ele:
+            key = max(elevation_gain.iterkeys(), key=(lambda key: elevation_gain[key]))
+        else:
+            key = min(elevation_gain.iterkeys(), key=(lambda key: elevation_gain[key]))
+    
+        return elevation_gain[key], self.getPathElevation(G, elevation_gain[key]), self.getPathLength(G, elevation_gain[key])
+    
+
+    def getDistFromPercentage(self,min_distance, percent):
+        if percent > 1:
+            return (percent) / 100.0 * min_distance
+        return (percent) * min_distance
 
     
     def findRoute(self, srcParams, destParams, percentage, boolIsMax):
@@ -44,7 +207,7 @@ class ProcessMap(object):
             raise Exception('INVALID SOURCE')
         
         if not self.isLocationValid(graph, destParams['latitude'], destParams['longitude']):
-            raise Exception('INVALID SOURCE')
+            raise Exception('INVALID DESTINATION')
         
         
         startNode = self.getNearestNode(graph, srcParams['latitude'], srcParams['longitude'])
--- RoutingMgr/ProcessRoute.py
+++ RoutingMgr/ProcessRoute.py
@@ -1,4 +1,6 @@
 import osmnx 
+import heapq
+import collections
 import pickle as pkl
 
 from osmnx.core import save_to_cache
@@ -18,7 +20,7 @@ Note this file is similar to the Algorithm,py file | copy paste the code over he
 class ProcessRoute(object):
 
     def __init__(self):
-        self.processMap = ProcessMap()
+        self.processMapObj = ProcessMap()
         
     
     def getRoute(self):
@@ -29,10 +31,10 @@ class ProcessRoute(object):
         boolIsMax = bool(payload['maxMin']=='max')
         percentage = float(payload['percentage'])
         
-        srcParams = self.processMap.processLocationParams(source)
-        destParams = self.processMap.processLocationParams(destination)
+        srcParams = self.processMapObj.processLocationParams(source)
+        destParams = self.processMapObj.processLocationParams(destination)
         
-        route, dist, elevation = self.processMap.findRoute(srcParams, destParams, percentage, boolIsMax)
+        route, dist, elevation = self.processMapObj.findRoute(srcParams, destParams, percentage, boolIsMax)
         
         respParams = dict()
         respParams['route'] = route
@@ -44,16 +46,140 @@ class ProcessRoute(object):
         return resp 
     
     
+#     
+#     
+#     def get_from_djikstra(G, start, end, percent, max_ele=True):
+#         min_distance = get_path_length(G, get_shortest_path(G, start, end))
+#         max_path_length = get_dis_from_percentage(min_distance, percent)
+#         candidate_paths = {}
+#         floored_percent = (percent / 10) * 10
+#         iters = []
+#         i = 100
+#         while (i <= floored_percent):
+#             iters.append(i)
+#             i += 10
+#             # print(i)
+#     
+#         for length in iters:
+#             pat_len = get_dis_from_percentage(min_distance, length)
+#             queue = []
+#             heappush(queue, (0, start))
+#             revPath = {}
+#             cost = {}
+#             cost_ele = {}
+#             revPath[start] = None
+#             cost[start] = 0
+#             cost_ele[start] = 0
+#             while len(queue) != 0:
+#                 (val, cur) = heappop(queue)
+#                 if cur == end:
+#                     if cost[cur] <= pat_len:
+#                         break
+#                 for cur, nxt, data in G.edges(cur, data=True):
+#                     cur_cost = cost[cur] + get_length(G, cur, nxt)
+#                     cur_ecost = cost_ele[cur]
+#                     ecost = get_elevation_gain(G, cur, nxt)
+#                     if ecost > 0:
+#                         cur_ecost = cur_ecost + ecost
+#                     if nxt not in cost or cur_cost < cost[nxt]:
+#                         cost_ele[nxt] = cur_ecost
+#                         cost[nxt] = cur_cost
+#                         if max_ele:
+#                             priority = -cur_ecost
+#                         else:
+#                             priority = cur_ecost
+#                         heappush(queue, (priority, nxt))
+#                         revPath[nxt] = cur
+#             path = generate_path(revPath, start, end)
+#             print(get_path_elevation(G, path))
+#             candidate_paths[get_path_elevation(G, path)] = path
+#     
+#         min_path_len = 10 ** 6
+#         max_path_len = 0
+#     
+#         for el in candidate_paths.keys():
+#             if el <= min_path_len:
+#                 min_path_len = el
+#             if el >= max_path_len:
+#                 max_path_len = el
+#     
+#         if max_ele:
+#             path = candidate_paths[max_path_len]
+#         else:
+#             path = candidate_paths[min_path_len]
+#     
+#         return get_lat_long(G, path), get_path_length(G, path), get_path_elevation(G, path)
+#     
+#         
     
 
-    def getPath(self, graph, startNode, endNode, percetage, boolIsMax):
-        # write function get_From_djikstra from lasamson
+    def getPath(self, graph, startNode, endNode, percetage, boolIsMax=True):
+        minDistance = self.processMapObj.getPathLength(graph, self.processMapObj.getShortestPath(graph, startNode, endNode))
+        #min_distance = get_path_length(G, get_shortest_path(G, start, end))
+        maxPathLength = self.processMapObj.getDistFromPercentage(minDistance, percetage)
+        #max_path_length = get_dis_from_percentage(min_distance, percent)
+        candidatePath = dict()
         
-        pass 
+        #candidate_paths = {}
+        #floored_percent = (percent / 10) * 10
+        iters = []
+        i = 100
+        while (i <= (percetage / 10) * 10):
+            iters.append(i)
+            i += 10
+            
+        for length in iters:
+            pat_len = self.processMapObj.getDistFromPercentage(minDistance, percetage)
+            queue = []
+            heapq.heappush(queue, (0, startNode))
+            revPath = {}
+            cost = {}
+            cost_ele = {}
+            revPath[startNode] = None
+            cost[startNode] = 0
+            cost_ele[startNode] = 0
+            while len(queue) != 0:
+                (val, cur) = heapq.heappop(queue)
+                if cur == endNode:
+                    if cost[cur] <= pat_len:
+                        break
+                for cur, nxt, data in graph.edges(cur, data=True):
+                    cur_cost = cost[cur] + self.processMapObj.getLength(graph, cur, nxt)
+                    cur_ecost = cost_ele[cur]
+                    ecost = self.processMapObj.getElevationGain(graph, cur, nxt)
+                    if ecost > 0:
+                        cur_ecost = cur_ecost + ecost
+                    if nxt not in cost or cur_cost < cost[nxt]:
+                        cost_ele[nxt] = cur_ecost
+                        cost[nxt] = cur_cost
+                        if boolIsMax:
+                            priority = -cur_ecost
+                        else:
+                            priority = cur_ecost
+                        heapq.heappush(queue, (priority, nxt))
+                        revPath[nxt] = cur
+            path = self.processMapObj.generatePath(revPath, startNode, endNode)
+            #print(get_path_elevation(G, path))
+            candidatePath[self.processMapObj.getPathElevation(graph, path)] = path
+    
+        min_path_len = 10 ** 6
+        max_path_len = 0
+    
+        for el in candidatePath.keys():
+            if el <= min_path_len:
+                min_path_len = el
+            if el >= max_path_len:
+                max_path_len = el
+    
+        if boolIsMax:
+            path = candidatePath[max_path_len]
+        else:
+            path = candidatePath[min_path_len]
+    
+    
+        getPathLatLong = self.processMapObj.getLatLong(graph, path)
+        getPathLength = self.processMapObj.getPathLength(graph, path)
+        getPathElevation = self.processMapObj.getPathElevation(graph, path)
         
-  
-    
-    
-    
-    
+        return getPathLatLong, getPathLength, getPathElevation
     

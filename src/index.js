b5aa7ca485f1732f4320b6ef62e8606a6dfe9b11
Dhruti patel <dhrutipatel5501@gmail.com>
2020-11-18 01:21:22 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DisplayUI from './StaticFiles/DisplayUI';
+
+ReactDOM.render(<DisplayUI/>, document.getElementById('ELENA_homepage'));
+

e4fd38c514c57a9747a5a0652a7602c3e4b934d8
Nitin Srinivasan <nitinsriniva@umass.edu>
2020-11-22 16:11:39 -0500

    added percentage of shortest path logic

--- src/RoutingMgr/Astar.py
+++ src/RoutingMgr/Astar.py
@@ -3,6 +3,8 @@ Created on Nov 13, 2020
 
 '''
 from src.MapMgr.ProcessMap import ProcessMap
+import heapq
+
 
 class Astar(object):
     '''
@@ -23,8 +25,53 @@ class Astar(object):
         return self.processMapObj.getElevationGain(graph, nodeA, nodeB)
 
 
+    def generatePath(self, revPath, start, end):
+        path = []
+        n = end
+        path.append(n)
+        while n != start:
+            n = revPath[n]
+            path.append(n)
+        return path[::-1]
+
+    def getShortestPath(self, G, start, end, option='length'):
+        queue = []
+        heapq.heappush(queue, (0, start))
+        revPath = {}
+        cost = {}
+        revPath[start] = None
+        cost[start] = 0
+
+        while len(queue) > 0:
+            (val, current) = heapq.heappop(queue)
+            if current == end:
+                break
+            for cur, nxt, data in G.edges(current, data=True):
+                cur_cost = cost[current]
+                if option == 'length':
+                    curCost = G.edges[cur, nxt, 0]['length']
+                elif option == 'elevation':
+                    curCost = self.processMapObj.getPathElevation(G, cur, nxt)
+                if curCost > 0:
+                    cur_cost += curCost
+                if nxt not in cost or cur_cost < cost[nxt]:
+                    cost[nxt] = cur_cost
+                    heapq.heappush(queue, (cur_cost, nxt))
+                    revPath[nxt] = current
+
+        return self.generatePath(revPath, start, end)
+
+
 
     def getRoute(self, graph, start, end, percentage, isMax=True):
+        # get the shortest path
+        minDistance = self.processMapObj.getPathLength(graph, self.getShortestPath(graph, start, end))
+        print("shortest path length: ", minDistance)
+
+        # Any path found should be less than this value
+        threshold = (percentage/100) * minDistance
+        print("threshold:", threshold)
+
         closed = set()
         open = set()
 
@@ -40,6 +87,9 @@ class Astar(object):
         node_costs[start]['g'] = 0
         node_costs[start]['h'] = 0
 
+        # for storing all the routes found
+        routes = []
+
 
         def getNextNode(nodes, node_costs, isMax):
             return max(nodes, key = lambda item: sum(node_costs[item].values())) if isMax else min(nodes, key = lambda item: sum(node_costs[item].values()))
@@ -51,41 +101,62 @@ class Astar(object):
             open.remove(current)
 
             if current == end:
-                path = []
-                while current is not None:
-                    path.append(current)
-                    current = parent[current]
-                path.reverse()
-                return self.processMapObj.getCoordinates(graph, path), self.processMapObj.getPathLength(graph, path), self.processMapObj.getPathElevation(graph, path)
+                if node_costs[current]['g'] <= threshold:
+                    path = []
+                    path_node = current
+                    while path_node is not None:
+                        path.append(path_node)
+                        path_node = parent[path_node]
+                    path.reverse()
 
-            closed.add(current)
+                    path_elevation = -1*self.processMapObj.getPathElevation(graph, path) if isMax else self.processMapObj.getPathElevation(graph, path)
+                    path_length = self.processMapObj.getPathLength(graph, path)
+                    path_details = self.processMapObj.getCoordinates(graph, path)
+                    print("found a path with length = " + str(node_costs[current]['g']) + " and elevation = " + str(path_elevation))
 
-            for neighbor in graph[current]:
-                if neighbor in closed:
-                    continue
+                    heapq.heappush(routes, (path_elevation, path_length, path_details))
 
-                if neighbor in open:
-                    old_neighbor_g_cost = node_costs[neighbor]['g']
-
-                    current_g_cost = node_costs[current]['g']
-                    current_move_cost = graph.edges[current, neighbor, 0]['length']
-
-                    new_neighbor_g_cost = current_g_cost + current_move_cost
-
-                    if old_neighbor_g_cost > new_neighbor_g_cost:
-                        node_costs[neighbor]['g'] = new_neighbor_g_cost
-                        parent[neighbor] = current
+                    #reset final node length
+                    node_costs[current]['g'] = float('inf')
                 else:
-                    current_g_cost = node_costs[current]['g']
-                    current_move_cost = graph.edges[current, neighbor, 0]['length']
+                    #print("found route but fails to satisfy threshold")
+                    #print("failed path length:", node_costs[current]['g'])
+                    #reset final node length
+                    node_costs[current]['g'] = float('inf')
+            else:
+                closed.add(current)
+                for neighbor in graph[current]:
+                    if neighbor in closed:
+                        continue
 
-                    neighbor_g_cost = current_g_cost + current_move_cost
-                    neighbor_h_cost = self.heuristic(graph, current, neighbor)
+                    if neighbor in open:
+                        old_neighbor_g_cost = node_costs[neighbor]['g']
 
-                    parent[neighbor] = current
-                    node_costs[neighbor] = {}
-                    node_costs[neighbor]['g'] = neighbor_g_cost
-                    node_costs[neighbor]['h'] = neighbor_h_cost
-                    open.add(neighbor)
+                        current_g_cost = node_costs[current]['g']
+                        current_move_cost = graph.edges[current, neighbor, 0]['length']
+
+                        new_neighbor_g_cost = current_g_cost + current_move_cost
+
+                        if old_neighbor_g_cost > new_neighbor_g_cost:
+                            node_costs[neighbor]['g'] = new_neighbor_g_cost
+                            parent[neighbor] = current
+                    else:
+                        current_g_cost = node_costs[current]['g']
+                        current_move_cost = graph.edges[current, neighbor, 0]['length']
+
+                        neighbor_g_cost = current_g_cost + current_move_cost
+                        neighbor_h_cost = self.heuristic(graph, current, neighbor)
+
+                        parent[neighbor] = current
+                        node_costs[neighbor] = {}
+                        node_costs[neighbor]['g'] = neighbor_g_cost
+                        node_costs[neighbor]['h'] = neighbor_h_cost
+                        open.add(neighbor)
+
+        if routes:
+            path_elevation, path_length, path_details = heapq.heappop(routes)
+            if isMax: path_elevation *= -1
+            return path_details, path_length, path_elevation
+
 
         raise ValueError('No path found between the start and end locations.')
